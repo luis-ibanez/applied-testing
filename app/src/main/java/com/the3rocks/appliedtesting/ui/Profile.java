f47bdf0e379d9e628e362d6e818cb00cd7151760
Luis Ibanez <luisibanez@Rises-MacBook-Pro.local>
2017-04-26 12:09:21 +0100

    Build empty project

---
+++ app/src/main/java/com/the3rocks/appliedtesting/ui/Profile.java
@@ -0,0 +1,15 @@
+package com.the3rocks.appliedtesting.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.the3rocks.appliedtesting.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
